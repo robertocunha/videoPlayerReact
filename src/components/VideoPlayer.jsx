caa9942c107526c5da3f7cea363df09d0159b005
Roberto Cunha <robertocunha@gmail.com>
2023-08-22 06:03:56 -0300

    fix: Garante que o elemento <video> seja atualizado

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,9 +1,14 @@
+import { useEffect, useRef } from "react"
+
 function VideoPlayer({ currentVideo }) {
+    const videoRef = useRef(null);
+
+    useEffect(() => { videoRef.current.load() }, [currentVideo])
 
     return (
         <>
             <h1>Video Player2</h1>
-            <video controls>
+            <video ref={videoRef} controls>
                 {currentVideo.map((video) =>
                     <source key={video.id} src={video.src} type={video.type} />
                 )}
